ed2f23d79a057377ff32734ab554f7c3cc495776
danielplebani <danielpleba03@gmail.com>
2023-08-31 21:06:57 +0200

    sistem all parianddispari game

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,66 +1,67 @@
 /*
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri.
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 */
 
-//dico all'utente di scegliere pari o dispari e nel caso scrive altro glielo faccio notare e torna indietro
+//dico all'utente di scegliere pari o dispari e nel caso scriva altro glielo faccio notare e torno indietro
 let wordCheck = true;
-let pariDispari; //dichiaro anche qua la variabile altrimenti non riesco a stabilire il vincitore finale
+let pariDispari;
 while (wordCheck) {
-    const pariDispari = prompt('scegli se vuoi pari o dispari!');
+    pariDispari = prompt('Scegli se vuoi pari o dispari!');
 
     if (pariDispari === 'pari' || pariDispari === 'dispari') {
-        console.log('hai scelto: ' + pariDispari);
+        console.log('Hai scelto: ' + pariDispari);
         wordCheck = false;
     } else {
-        alert('scegli o pari o dispari!!!')
+        alert('Scegli o pari o dispari!!!');
         wordCheck = true;
     }
 }
 
 //dico all'utente di scegliere un numero da 1 a 5 e nel caso scriva altro, glielo faccio notare e torno indietro
 let numberCheck = true;
-let userNumber; //dichiaro anche qua la variabile altrimenti non riesco a fare la somma
+let userNumber;
 while (numberCheck) {
-   userNumber = Number(prompt('scegli un numero da 1 a 5!'));
-   
-   if (userNumber > 5 || userNumber < 1) {
-       alert('scegli un numero da uno a cinque!!!');
-       numberCheck = true;
-   } else {
-       console.log('hai scelto il numero: ' + userNumber);
-       numberCheck = false;
-   }
-}
+    userNumber = Number(prompt('Scegli un numero da 1 a 5!'));
 
-//il computer genera un numero random da 1 a 5
-function computerFunction(number) {
-    return number;
-}
-const computerMarkup = Math.floor(Math.random() * 5 + 1);
-const computerNumber = computerFunction(computerMarkup);
-console.log('il numero random generato dal computer è: ' + computerNumber);
-
-
-//sommo e stabilisco se la somma è pari o dispari
-let somma = false;
-function sommaNumber(somma) {
-    console.log('la somma dei due numeri è: ' + somma);
-
-    if (somma % 2 === 0) {
-        console.log('il numero vincente sarà pari');
-        somma = true;
+    if (userNumber > 5 || userNumber < 1 || isNaN(userNumber)) {
+        alert('Scegli un numero da 1 a 5!!!');
+        numberCheck = true;
     } else {
-        console.log('il numero vincente sarà dispari');
-        somma = false;
+        console.log('Hai scelto il numero: ' + userNumber);
+        numberCheck = false;
     }
 }
 
-sommaNumber(userNumber + computerNumber);
+//il computer genera un numero random da 1 a 5
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+const computerNumber = generateRandomNumber();
+console.log('Il numero random generato dal computer è: ' + computerNumber);
+
+//sommo e stabilisco se la somma è pari o dispari
+let pariDispariWinner;
+function checkPariDispari(somma) {
+    if (somma % 2 === 0) {
+        console.log('La somma dei due numeri è pari.');
+        pariDispariWinner = (pariDispari === 'pari');
+    } else {
+        console.log('La somma dei due numeri è dispari.');
+        pariDispariWinner = (pariDispari === 'dispari');
+        pariDispariWinner = false;
+    }
+
+    console.log(pariDispariWinner);
+}
+
+const somma = userNumber + computerNumber;
+console.log('La somma dei due numeri è: ' + somma);
+checkPariDispari(somma);
 
 //controllo cosa aveva scelto l'utente e stabilisco il vincitore
-if ((pariDispari === 'pari' && somma === true) || (pariDispari === 'dispari' && somma === false)) {
+if (pariDispariWinner) {
     console.log('HAI VINTO!!!');
     document.querySelector('h1').innerHTML = 'HAI VINTO!!!';
 } else {
